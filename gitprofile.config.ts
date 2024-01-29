8a0d847c63de5629577c2166493b4a533b286cfa
Jerry J <78483514+jerryjarvela@users.noreply.github.com>
2024-01-29 15:10:38 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -17,10 +17,10 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 6, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['jerryjarvela/jerryjarvela.github.io'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
@@ -52,90 +52,99 @@ const CONFIG = {
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Jerry Järvelä',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'jerryjarvela',
+    twitter: '',
+    mastodon: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: 'https://jerryjarvela.github.io/',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'jerryjarvela@outlook.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
+    'C#',
+    'Unity',
     'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Github',
+    'Zenhub',
+    'SCRUM',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
-    },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Ticorporate 2022',
+      position: 'Game Programmer, "Git Guy"',
+      from: 'August 2022',
+      to: 'December 2022',
+      companyLink: 'https://store.steampowered.com/app/2200470/Ok_I_Pull_Up/',
     },
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: 'GameDevTV Complete C# Unity Game Developer 3D Online Course',
+      body: '',
+      year: 'August 2021',
+      link: 'https://www.gamedev.tv/',
+    },
+    {
+      name: 'GameDevTV Complete C# Unity Game Developer 2D Online Course',
+      body: '',
+      year: 'February 2021',
+      link: 'https://www.gamedev.tv/',
+    },
+    {
+      name: 'GameDevTV Get Git Smart Course: Learn Git in Unity, SourceTree, GitHub',
+      body: '',
+      year: 'February 2021',
+      link: 'https://www.gamedev.tv/',
+    },
+    {
+      name: 'GameDevTV Unity RPG Core Combat Creator: Learn Intermediate C# Coding',
+      body: '',
+      year: 'August 2021',
+      link: 'https://www.gamedev.tv/',
+    },
+    {
+      name: 'GameDevTV Unity RPG Inventory Systems: Intermediate C# Game Coding Course',
+      body: '',
+      year: 'August 2021',
+      link: 'https://www.gamedev.tv/',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'Axxell',
+      degree: 'Undergraduate Degree in Information and communications technology',
       from: '2015',
-      to: '2019',
+      to: '2018',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Jamk Universify of Applied Sciences',
+      degree: 'Bachelors Degree Programme in Business Information Technology',
+      from: '2020',
+      to: 'Present',
     },
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 3, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
