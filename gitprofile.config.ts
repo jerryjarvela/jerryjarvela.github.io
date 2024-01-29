078202d97eb1001ac13d36a25262d9cd49ba8f61
Jerry J <78483514+jerryjarvela@users.noreply.github.com>
2024-01-29 15:29:47 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -156,7 +156,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'procyon',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
